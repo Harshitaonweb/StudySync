3e5ee71f367b939f19be0539a28651604310ad41
Harshitaonweb <harshitaonweb@gmail.com>
2026-05-21 21:30:45 +0530

    bug fix

--- server/index.js
+++ server/index.js
@@ -16,7 +16,15 @@ const app = express();
 
 // Security & parsing
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({
+  origin: [
+    process.env.CLIENT_URL,
+    'https://study-sync-eight-psi.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:5174',
+  ].filter(Boolean),
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
