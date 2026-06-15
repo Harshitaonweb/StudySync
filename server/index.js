d1cb8b3d11a7055bd9634c7cdc483091eed0d232
Harshitaonweb <harshitaonweb@gmail.com>
2026-06-15 20:53:49 +0530

    domain edited

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => logger.info(`Server running on port ${PORT}`));
 
 module.exports = app;
